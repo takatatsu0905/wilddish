0b0cded294d17ed3b8dcd3af97784932dfb2e73a
takumi <wthkxx3020@gmail.com>
2021-12-26 23:05:48 +0900

    レシピ登録画面 画像パス保存

--- wilddish_laravel/database/migrations/2021_12_22_142446_create_recipes_table.php
+++ wilddish_laravel/database/migrations/2021_12_22_142446_create_recipes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateRecipesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('recipes', function (Blueprint $table) {
             $table->increments('id');
             $table->string('status')->default('active');
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
-            $table->string('image_name');
-            $table->text('serf_introduction');
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('title');
+            $table->integer('tool_id')->unsigned()->index();
+            $table->string('ingredients');
+            $table->text('make')->nullable();
+            $table->string('image_name')->nullable();
             $table->timestamps();
         });
     }
@@ -32,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('recipes');
     }
 }
--- wilddish_laravel/database/migrations/2021_12_22_212958_create_processes_table.php
+++ wilddish_laravel/database/migrations/2021_12_22_212958_create_processes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProcessesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('processes', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('status')->default('active');
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
+            $table->integer('recipe_id')->unsigned()->index();
+            $table->string('process_title');
+            $table->text('make');
             $table->string('image_name');
-            $table->text('serf_introduction');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('processes');
     }
 }
