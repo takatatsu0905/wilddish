c04342194bf35dc6b0f6e9970abf9463c3b82cce
suda Hiroshi <sutaruhin@gmail.com>
2022-02-19 00:19:58 +0900

    検索対応版　須田

--- wilddish_laravel/app/Http/Controllers/SearchController.php
+++ wilddish_laravel/app/Http/Controllers/SearchController.php
@@ -86,57 +86,30 @@ class SearchController extends Controller
         //
     }
     public function search(Request $request)
-  {
-    $tools = Tool::all();
+    {
+        $tools = Tool::all();
 
-    $serach=$request->input('q');
+        $serach=$request->input('q');
 
+        $toolId = $request->tools;
+        $query = Recipe::query();
+        $query->whereHas('tools', function($q) use($toolId)  {
+            $q->whereIn('recipe_tool.tool_id', $toolId);
+        });
+        if(isset($serach) && $serach != "") {
+            $query->where('title', 'like', '%'.$serach.'%');
+        }
+    // dd($query->get());
+    //     $query=DB::table('recipes');
+    //     //検索ワードの全角スペースを半角スペースに変換
+    //     $serach_spaceharf=mb_convert_kana($serach, 's');
+    //     //検索ワードを半角スペースで区切る
+    //     $keyword_array=preg_split('/[\s]+/', $serach_spaceharf, -1, PREG_SPLIT_NO_EMPTY);
+    //     $query->where('title', 'like', '%'.$serach.'%')->whereIn('tool_id',$request->tools);
+    //     $query->select('id','user_id','image_name', 'title','tool_id','ingredients');
+    //     //   $query->select('id','user_id','image_name', 'title','tool_id','ingredients');
 
-
-    // $tool1=$request->input('tool1');
-
-    // $tool2=$request->input('tool2');
-
-    // $tool3=$request->input('tool3');
-
-    // $tool4=$request->input('tool4');
-
-
-    $toolId = $request->tools;
-    $query = Recipe::query();
-    $query->whereHas('tools', function($q) use($toolId)  {
-        $q->whereIn('recipe_tool.tool_id', $toolId);
-    });
-    if(isset($serach) && $serach != "") {
-        $query->where('title', 'like', '%'.$serach.'%');
+        $recipes=$query->paginate(20);
+        return view('recipes/list',compact('recipes','tools'));
     }
-dd($query->get());
-
-
-    $query=DB::table('recipes');
-
-    //検索ワードの全角スペースを半角スペースに変換
-    $serach_spaceharf=mb_convert_kana($serach, 's');
-
-
-    //検索ワードを半角スペースで区切る
-    $keyword_array=preg_split('/[\s]+/', $serach_spaceharf, -1, PREG_SPLIT_NO_EMPTY);
-
-
-
-    $query->where('title', 'like', '%'.$serach.'%')->whereIn('tool_id',$request->tools);
-
-    $query->select('id','user_id','image_name', 'title','tool_id','ingredients');
-
-
-
-    //   $query->select('id','user_id','image_name', 'title','tool_id','ingredients');
-
-
-    $recipes=$query->paginate(20);
-
-
-
-    return view('recipes/list',compact('recipes','tools'));
-  }
 }
